eaf0efb3755035a825b18ac9e2b04c5fd8d7251f
Japagabriel <gcanutosantos@gmail.com>
2022-11-24 19:22:41 -0300

    Aula 1 á 9

---
+++ src/aula9.rs
@@ -0,0 +1,39 @@
+use std::io;
+
+// nome da variável 'convert_to_int' vai pegar a variável data_input e devolver em numero inteiro 'i32'
+
+fn convert_to_int(data_input: & String) -> i32 { 
+
+    /*criamos a variável 'x' que irá pegar o valor de 'data_input' (string) e aplicar o método '.trim()' que irá 
+    cortar a string no próximo ' ', logo utilizar o método .parse::<i32>() para converter esse valor em um tipo inteiro
+    'i32' e por fim utilizará o '.unwrap()' para pegar o valor e se tiver um 'none' dentro do valor, irá retornar o erro.
+
+    */    
+    let x = data_input.trim().parse::<i32>().unwrap();
+
+    // retorna o valor de 'x' ao final de todo o processo.
+    x
+
+}
+
+fn main(){  
+    // criou a variável 'number1' de tipo String e reservou um espaço de memória para ela. 
+    let mut number1 = String::new();
+    
+    /*irá fazer a entrada do usuário, '.read_line' irá alocar a entrada de usuário no 'number1', passando a 
+    caracteristica da váriável como mutável e .expect()irá fazer um tratamento de excessão para caso ocorra um erro.*/
+    io::stdin().read_line(&mut number1).expect("Erro ao ler Number1");
+
+    let mut number2 = String::new();
+    io::stdin().read_line(&mut number2).expect("Erro ao ler Number2");
+
+    // o '(&number1)' está alterando o valor está também alterando o tipo dele.
+    if convert_to_int(&number1) > convert_to_int(&number2){
+        println!("{} > {}", number1, number2);
+    }else if convert_to_int(&number1) == convert_to_int(&number2){
+        println!("{} = {}", number1, number2);
+    }else{ 
+        println!("{} > {}", number2, number1);
+
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,30 @@
+/*Na Linguagem RUST é de suma importância todas as variáveis e identações ser feitas em letra minuscula, caso contrário, 
+irá gerar erro de sintaxe "convert the identifier to snake case:".
+
+*/ 
+fn main() {
+    // para definir uma variável é necessário ': &tipo_da_variável =' caso contrário retornará erro!
+    let _nome: &str = "Gabriel"; // let define a variável estática, ou seja, não mutável e impossibilitando uma nova atribuição ou troca de valores
+    let mut _comida: &str = "eu sou"; // let + mut torna a variável mutável, ou seja, agora sim pondemos alterar e atribuir novos valores. 
+    _comida = "seu almoço";
+    println!("Hello, {}!", _nome); // as {} definem o espaço onde irá declarar o lugar da variável, separando por fora com ',' e chamando a variável 'nome' para preencher.
+    println!("Ei... {}",_comida); 
+    let _i: i16 = 90; //o termo 'i' define como valor tamanho de bit'16' em BCD com maior capacidade de representatividade '-32768' até '32767'.
+    let _x: u8 = 70; //o termo 'u' define a varável como tamanho de bit'8' de forma polinômica com menor representatividade '0' á '255'.
+    let _y: bool = true; //o termo 'bool' define a váriável com valor Booleano, ou seja, 'true' ou 'false'.
+    let _z: f64 = 56.304553434; // o termo 'f' define a váriável com valor quebrado, ou seja, 'float'.
+
+
+    let numb1 =43; // variáveis de controle utilizadas no 'if' e 'else' a seguir...
+    let numb2 =43; 
+
+    if numb1 > numb2 {
+        println!("{} > {}", numb1,numb2); // declaração de fluxo de controles com 'if' e 'else', espressando condições
+    
+    }else if numb1 == numb2 {
+        println!("{} = {}", numb1,numb2);
+    }else{
+        println!("{} > {}", numb2,numb1);
+    }
+
+}
